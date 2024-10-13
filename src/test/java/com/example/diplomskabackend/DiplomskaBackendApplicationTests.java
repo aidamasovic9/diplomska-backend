8aac2569467fd492c72c454cd779c7680da1413b
Aida Mashovikj <Aida.Mashovikj@netcetera.com>
2024-10-13 15:23:10 +0200

    Initial commit

---
+++ src/test/java/com/example/diplomskabackend/DiplomskaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomskabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomskaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
